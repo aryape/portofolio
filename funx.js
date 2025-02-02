df3f00bee4120b7163cef66492a769556dca15e5
aryaradipta13 <aryaradipta13@gmail.com>
2025-02-02 16:47:56 +0800

    update button music

--- funx.js
+++ funx.js
@@ -36,3 +36,34 @@ const writeLoop = async () => {
 };
 
 writeLoop();
+
+    const audio = document.querySelector('#audiox')
+    const audioPlayer = document.querySelector('.audio_player');
+    const playBtn = document.querySelector('#audio_play');
+
+    // check if song is playing
+    function isAudioPlaying() {
+        return audioPlayer.classList.contains('playing');
+    }
+
+    // play audio of current song
+    function playAudio() {
+        audioPlayer.classList.add('playing');
+        playBtn.querySelector('i').classList.remove('ri-play-circle-fill');
+        playBtn.querySelector('i').classList.add('ri-pause-circle-line');
+        audio.play();
+    }
+
+    // pause audio of current song
+    function pauseAudio() {
+        audioPlayer.classList.remove('playing');
+        playBtn.querySelector('i').classList.add('ri-play-circle-fill');
+        playBtn.querySelector('i').classList.remove('ri-pause-circle-line');
+        audio.pause();
+    }
+
+    playBtn.addEventListener('click', () => {
+        isAudioPlaying() ? pauseAudio() : playAudio();
+    });
+
+  
